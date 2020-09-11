003483c65249902a849cb40f0b157c41fb723713
user1 <user1@user1-PC>
2020-09-11 09:05:47 +0900

    보드  수정   0911

--- sogonsogon/src/main/java/com/kh/sogon/board/controller/BoardController.java
+++ sogonsogon/src/main/java/com/kh/sogon/board/controller/BoardController.java
@@ -1,13 +1,20 @@
 package com.kh.sogon.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.kh.sogon.board.model.service.BoardService;
 
 @SessionAttributes({"loginMember"})
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
+	
+	
+	@Autowired
+	private BoardService BoardService;
 	
 	   @RequestMapping("boardList")
 	   public String boardListView() {
